4473b7002e59539e821a9208d02bd156a32b765a
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2021-01-03 20:08:22 +0900

    feat: implement signup, tweet

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.users = {}  # 가입한 사용자 저장 key = id: int, value = new_user: JSON
+app.id_count = 1
+
+
+@app.route("/ping", methods=['GET'])
+def ping() -> str:
+    return "pong"
+
+
+# 회원가입
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json  # JSON -> dict
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1
+    return jsonify(new_user)  # dict -> JSON
+
+
+# 트윗 구현
+app.tweets = []
+
+
+@app.route('/tweet', methods=['POST'])
+def tweet() -> tuple:
+    payload = request.json
+    user_id = int(payload["id"])
+    tweet = payload["tweet"]
+
+    if user_id not in app.users:
+        return "사용자가 존재하지 않습니다", 400
+
+    if len(tweet) > 300:
+        return "300자를 초과했습니다", 400
+
+    user_id = int(payload['id'])
+    app.tweets.append({
+        "user_id": user_id,
+        "tweet": tweet
+    })
+
+    return '', 200
