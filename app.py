cb82bdc4dfa6a3cd15d799f17e9820c9219d139c
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2021-01-03 21:48:37 +0900

    feat: implement follow, unfollow

--- app.py
+++ app.py
@@ -43,3 +43,19 @@ def tweet() -> tuple:
     })
 
     return '', 200
+
+
+# 팔로우, 언팔로우
+@app.route('/follow', methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return "사용자가 존재하지 않습니다", 400
+
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_id_to_follow)
+
+    return jsonify(user)
